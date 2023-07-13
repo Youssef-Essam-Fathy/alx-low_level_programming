fa68b2b8d5a32c3f70fd19176a699be10aa5cfac
Youssef <youssef@email>
2023-07-12 23:28:48 -0700

    C programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Size is not grandeur, and territory does not make a nation'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte(s)" ,size(char));
+	return (0);
+}
