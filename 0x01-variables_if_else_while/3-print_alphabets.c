b48254231d2f479a28fbae2379f224b59c8d9b0f
Youssef <youssef@email>
2023-07-14 08:54:26 -0700

    Variables in C programming

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,7 +10,6 @@
  * Description: 'the program's description'
  * Return: Always 0 (Success)
  */
-/* betty style doc for function main goes there */
 
 int main(void)
 {
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -13,16 +13,14 @@
 
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
-		printf("%d is zero\n", n);
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
 	return (0);
 }
