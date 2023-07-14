669036d0fa6ff9ef185d2f8ed84a52b9390ba290
Youssef <youssef@email>
2023-07-14 09:23:31 -0700

    Variables in C programming

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* your code goes there */
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
