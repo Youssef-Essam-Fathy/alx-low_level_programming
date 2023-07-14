cec02fc9594f5826910452429c655d8febb59a49
Youssef <youssef@email>
2023-07-14 05:46:42 -0700

    Variables in C programming

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	/* your code goes there */
+	char ch = 'a';
+	char ch2 = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (ch2 <= 'Z')
+	{
+		putchar(ch2);
+		ch2++;		
+	}
+
+	putchar('\n');
+	return (0);
+}
