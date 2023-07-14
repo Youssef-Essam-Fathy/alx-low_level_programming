60396b82b2eff42b55aa07f4f122bf620a2250d7
Youssef <youssef@email>
2023-07-14 11:14:52 -0700

    Variables in C programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,22 +14,22 @@
 int main(void)
 {
 	/* your code goes there */
-	int num1 = 48, num2;
+	int num1 = 0, num2;
 
-	while (num1 <= 57)
+	while (num1 <= 9)
 	{
-		num2 = 48;
-		while (num2 <= 57)
+		num2 = 0;
+		while (num2 <= 9)
 		{
 			if (num1 != num2 && num1 < num2)
 			{
-				putchar(num1);
-				putchar(num2);
-			if (num1 != 8 || num2 != 9)
-			{
-			putchar(',');
-			putchar(' ');
-			}
+				putchar(num1 + 48);
+				putchar(num2 + 48);
+				if (num1 + num2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 			num2++;
 		}
