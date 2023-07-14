e55f218438eef80849b3ad59d4c1f0c0fe7c2733
Youssef <youssef@email>
2023-07-13 21:16:29 -0700

    C programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
 /**
  * main - Entry point
  * Description: 'the program's description'
  * @parameter: describe the parameter
  * Return: Always 0 (Success)
  */
-/* more headers goes there */
 /* betty style doc for function main goes there */
+
 int main(void)
 {
 	int n;
