b113cbe261d5c33afb9809ce627f17593120ca41
Youssef <youssef@email>
2023-07-14 08:57:31 -0700

    Variables in C programming

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* your code goes there */
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
