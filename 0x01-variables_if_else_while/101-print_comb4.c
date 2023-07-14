dc630e32f68aa7e9ee06246280f105cb71a35438
Youssef <youssef@email>
2023-07-14 11:21:42 -0700

    Variables in C programming

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* your code goes there */
+	int num1 = 0, num2, num3;
+
+	while (num1 <= 9)
+	{
+		num2 = 0;
+		while (num2 <= 9)
+		{
+			num3 = 0;
+			while (num3 <= 9)
+			{
+			if (num1 != num2 && num1 < num2 && num2 < num3)
+			{
+				putchar(num1 + 48);
+				putchar(num2 + 48);
+				putchar(num3 + 48);
+				if (num1 + num2 + num3 != 24)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			num3++;
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}
