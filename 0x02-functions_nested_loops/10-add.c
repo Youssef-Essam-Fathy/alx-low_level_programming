e1250caaae0f2bb2ba066b7f804f8eb3ddc07934
Youssef <youssef@email>
2023-07-18 15:07:29 -0700

    Functions and nested_loops in C programming

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - a function that prints the sum of 2 numbers
+ *
+ *@n1: takes in integer type input for a function
+ *@n2: takes in integer type input for a function
+ *
+ *Return: sum of two integers
+ *
+ **/
+
+int add(int n1, int n2)
+{
+	int result;
+
+	result = n1 + n2;
+	return (result);
+}
