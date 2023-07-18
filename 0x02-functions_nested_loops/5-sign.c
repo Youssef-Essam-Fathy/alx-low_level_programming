5b8ef0ac84c855d9c98799817e2a8d723263c29a
Youssef <youssef@email>
2023-07-18 11:20:48 -0700

    Functions and nested_loops in C programming

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,8 +13,19 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		return (1);
+		putchar('+');
+	}
 	else if (n < 0)
+	{
 		return (-1);
-	return (0);
+		putchar('-');
+	}
+	else
+	{
+		return (0);
+		putchar('0');
+	}
+	putchar('\n');
 }
