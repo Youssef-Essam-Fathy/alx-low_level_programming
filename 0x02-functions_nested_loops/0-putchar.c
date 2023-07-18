4bad1b690b517a1ac9e16895378bc82fef4da7d3
Youssef <youssef@email>
2023-07-17 20:35:26 -0700

    Variables in C programming

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'A program to print a string output'
+ *@printf: prints a formated output
+ *
+ * Return: Always 0 (success)
+ **/
+
+int main(void)
+{
+	printf("__putchar\n");
+	return (0);
+}
