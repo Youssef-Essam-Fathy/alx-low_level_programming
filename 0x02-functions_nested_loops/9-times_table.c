3fa0d74b35462a749a390e1e576ad0c1d7e803d3
Youssef <youssef@email>
2023-07-18 14:07:44 -0700

    Functions and nested_loops in C programming

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,14 +8,14 @@ void times_table(void)
 {
 	int n1, n2;
 
-	for (n1 = 0; n1 <= 10; n1++)
+	for (n1 = 0; n1 <= 9; n1++)
 	{
-		for (n2 = 0; n2 <= 10; n2++)
+		for (n2 = 0; n2 <= 9; n2++)
 		{
 			_putchar((n1 * n2) + 48);
 			_putchar(',');
 			_putchar(' ');
-			_putchar('\n');
 		}
+			_putchar('\n');
 	}
 }
