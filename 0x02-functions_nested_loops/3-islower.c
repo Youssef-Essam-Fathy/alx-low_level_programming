532b09f970c6bd754efd30eea45f7eaa18a55032
Youssef <youssef@email>
2023-07-18 08:28:57 -0700

    Functions and nested_loops in C programming

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,15 @@
 /**
  *_islower - use the _islower function to check
  *              for lowercase characters
+ *@c: a function input checker
+ *
+ *Return: returns 1 if 'c' is lowercase
+ *         otherwise always 0 (Success)
  **/
 
 int _islower(int c)
 {
-if (c >= 'a' && c <= 'z' )
+if (c >= 'a' && c <= 'z')
 return (1);
 return (0);
 }
