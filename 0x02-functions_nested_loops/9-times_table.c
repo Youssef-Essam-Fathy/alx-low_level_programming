06d5701d23997a4a528b63dd8de9915560a6cacd
Youssef <youssef@email>
2023-07-18 15:00:40 -0700

    Functions and nested_loops in C programming

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,15 +6,26 @@
 
 void times_table(void)
 {
-	int n1, n2;
+	int num, mult, prod;
 
-	for (n1 = 0; n1 <= 9; n1++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (n2 = 0; n2 <= 9; n2++)
+		_putchar(48);
+		for (mult = 1; mult <= 9; mult++)
 		{
-			_putchar((n1 * n2) + 48);
 			_putchar(',');
 			_putchar(' ');
+
+			prod = num * mult;
+			/*
+			 * put space if product is single number
+			 * place the first digit if it is two numbers
+			 */
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + 48); /*get the first digit*/
+			_putchar((prod % 10) + 48); /*get the second digit*/
 		}
 			_putchar('\n');
 	}
