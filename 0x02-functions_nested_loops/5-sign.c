3111bd8aa5de8c904232c46bf191d43c1fd36faa
Youssef <youssef@email>
2023-07-18 11:29:38 -0700

    Functions and nested_loops in C programming

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,7 @@
  *              the sign for a number
  *@n: a function input checker
  *
- *Return: returns 1 if 'n' is greater than '0'
+ *Return: returns +1 if 'n' is greater than '0'
  *         returns -1 if 'n' is less than '0'
  *         otherwise always 0 (Success)
  **/
@@ -14,18 +14,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
-		putchar('+');
+		return ('+'1);
 	}
 	else if (n < 0)
 	{
-		return (-1);
-		putchar('-');
+		return ('-'1);
 	}
 	else
 	{
-		return (0);
-		putchar('0');
+		return ('0'0);
 	}
 	putchar('\n');
 }
