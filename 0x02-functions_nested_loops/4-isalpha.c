9c7d62143406399d5ead94ae211873e555008995
Youssef <youssef@email>
2023-07-18 10:39:56 -0700

    Functions and nested_loops in C programming

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,11 +11,11 @@
 
 int _isalpha(int c)
 {
-if (c >= 'A'  c <= 'Z')
+if (c >= 'A' &&  c <= 'Z')
 {
 	return (1);
 }
-else if (c >= 'a' c <= 'z')
+else if (c >= 'a' && c <= 'z')
 {
 	return (1);
 }
