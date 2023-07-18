0403e8d3a6d6c412578b31b11f104f57cd9fb893
Youssef <youssef@email>
2023-07-18 09:26:29 -0700

    Functions and nested_loops in C programming

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_isalpha - use the _islower function to check
+ *              for lowercase characters
+ *@c: a function input checker
+ *
+ *Return: returns 1 if 'c' is a letter
+ *         otherwise always 0 (Success)
+ **/
+
+int _isalpha(int c)
+{
+if (c >= 'A' && c <= 'Z')
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (1);
+return (0);
+}
