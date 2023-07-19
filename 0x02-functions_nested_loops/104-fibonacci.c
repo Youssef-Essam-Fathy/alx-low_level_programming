95c4d9260e974545ae89292f872b856fef1fb1b4
Youssef <youssef@email>
2023-07-18 17:38:40 -0700

    Functions and nested_loops in C programming

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -35,7 +35,7 @@ int numLength(int num)
 int main(void)
 {
 	int count, initial0s;
-	unsigned long f1 = 0, f2 = 1, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
 
 	for (count = 1; count <= 98; count++)
 	{
