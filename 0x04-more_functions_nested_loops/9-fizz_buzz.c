4291c7772ecb9f5c73dc33cc4483bd4b60341947
Youssef <youssef@email>
2023-07-20 10:50:48 -0700

    More functions_nested_loop in C programming

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,15 +13,18 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3) == 0 && (i % 5) == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if ((i % 3) == 0 && !(i % 5 == 0))
-			printf("Fizz ");
+			printf("Fizz");
 		else if ((i % 5) == 0 && !(i % 3 == 0))
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
 	}
-	printf("\n");
+	if (i != 100)
+		printf(" ");
+	else
+		printf("\n");
 
 	return (0);
 }
