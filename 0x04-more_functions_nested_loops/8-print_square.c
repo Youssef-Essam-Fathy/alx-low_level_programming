321e30b1ac91a9e8d3ef1cb39cf253469d80be92
Youssef <youssef@email>
2023-07-20 08:56:27 -0700

    More functions_nested_loop in C programming

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_square - prints a square in the terminal
+ *
+ *@size: size of the square
+ *
+ * Return: void
+ **/
+
+void print_square(int size)
+{
+	int row, column;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (column = 1; column <= size; column++)
+			_putchar(35);
+		_putchar('\n')
+	}
+}
