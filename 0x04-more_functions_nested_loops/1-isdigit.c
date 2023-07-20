b84543996a2bc4d293ce95408887049a0c52a662
Youssef <youssef@email>
2023-07-19 21:46:55 -0700

    More functions_nested_loop in C programming

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_isdigit - checks for a digit (0 through 9)
+ *
+ *@c: an integer input
+ *
+ * Return: 1 in case uppercase letter
+ *	always 0 (Success)
+ **/
+
+int _isdigit(int c)
+{
+	for (c = 48; c <= 57; c++)
+	{
+		return (1);
+	}
+	return (0);
+}
