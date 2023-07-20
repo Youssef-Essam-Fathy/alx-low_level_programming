ee9a3df235a5986d266fe96eb6063cb3628e52ca
Youssef <youssef@email>
2023-07-19 21:51:21 -0700

    More functions_nested_loop in C programming

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *mul -  multiplies two integers
+ *
+ *@a: an integer input
+ *@b: an integer input
+ *
+ * Return: 1 in case uppercase letter
+ *	always 0 (Success)
+ **/
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
