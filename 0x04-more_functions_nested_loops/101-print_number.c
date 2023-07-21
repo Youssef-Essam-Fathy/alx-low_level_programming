074fb188891d52262e0f8016254715096b372104
Youssef <youssef@email>
2023-07-20 18:07:19 -0700

    More functions_nested_loop in C programming

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,5 +10,19 @@
 
 void print_number(int n)
 {
-	_putchar(n + 48);
+	unsigned int num = n;
+
+	/*1st check negativity*/
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	/*print 1st few digits*/
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	/*prints last digit*/
+	_putchar((num % 10) + 48);
 }
