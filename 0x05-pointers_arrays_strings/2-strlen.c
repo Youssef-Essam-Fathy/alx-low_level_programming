32397dbef1046e6b2e2fe7da5511773f2406794c
Youssef <youssef@email>
2023-07-24 13:32:56 -0700

    Debugging in C programming

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the string length
+ *
+ * @s: character pointer = string
+ *
+ * Return: integer value
+ **/
+
+int _strlen(char *s)
+{
+	size_t strlen(s);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <string.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
