134714316c30e7c13d587e9c4338820473ab910c
Youssef <youssef@email>
2023-07-24 15:02:22 -0700

    Debugging in C programming

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts -  prints a string
+ *
+ * @str: A pointer
+ *
+ * Return: void
+ **/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <string.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
