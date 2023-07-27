6dd5324057d77b2a10ba2580991fd4f985517427
Youssef <youssef@email>
2023-07-26 21:03:51 -0700

    Debugging in C programming

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: firdt pointer parameter
+ * @s2: second pointer parameter
+ *
+ * Return: an integer value and 0 in case s1 = s2
+ **/
+
+int _strcmp(char *s1, char *s2)
+{
+	int equal = 0;
+
+	while (*s1)
+	{
+		if (*s1 != *s2)
+		{
+			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		s1++;
+		s2++;
+
+		return (equal);
+	}
+}
