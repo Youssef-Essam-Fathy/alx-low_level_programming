5b041887eb4d6fe822db7eff5d813710e84f7386
Youssef <youssef@email>
2023-07-26 19:33:18 -0700

    Debugging in C programming

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: character pointer input parameter
+ * @src: character pointer input parameter
+ * @n: integer input parameter
+ *
+ * Return: a character value
+ **/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, i2;
+
+	i = 0;
+
+	/*find the size of dest*/
+	while (dest[i])
+		i++;
+
+	for (i2 = 0; i2 < n && src[i2] != '\0'; i2++)
+		dest[i] = src[i2];
+
+	return (dest);
+}
