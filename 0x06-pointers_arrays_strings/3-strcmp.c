d1c82cc8b2ca98058230475e29855594e9f9e9b3
Youssef <youssef@email>
2023-07-27 06:17:09 -0700

    Debugging in C programming

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,12 +17,11 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (*s1 != *s2)
 		{
-			equal = ((int)*s1 + 48) - ((int)*s2 + 48);
+			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
 			break;
 		}
 		s1++;
 		s2++;
-
-		return (equal);
 	}
+	return (equal);
 }
