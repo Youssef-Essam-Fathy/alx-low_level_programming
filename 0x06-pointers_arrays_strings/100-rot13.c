d0c688204bf1d8ac93b6153e71254df26d1ef9ac
Youssef <youssef@email>
2023-07-27 14:34:01 -0700

    Debugging in C programming

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes the string using rot13
+ *
+ * @s: string
+ *
+ * Return: a character value
+ **/
+
+char *rot13(char *s)
+{
+	int i;
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *ptr = s;
+
+	while (*s)
+	{
+		for (i = 0; i <= 52; i++)
+		{
+			if (*s == rot13[i])
+			{
+				*s = ROT13[i];
+				break;
+			}
+		}
+		s++;
+	}
+
+	return (ptr);
+}
