4cac472aed8591345b65779fb1ec2257ca1ed053
Youssef <youssef@email>
2023-07-27 06:28:31 -0700

    Debugging in C programming

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,12 +11,12 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, j;
+	int i, j, t;
 
 	for (i = 0; j = (n - 1); i < j; i++, j--)
 	{
-		a[i] = a[i] + a[j];
-		a[j] = a[i] - a[j];
-		a[i] = a[i] - a[j];
+		t = a[i];
+		a[i] = a[j];
+		a[j] = t;
 	}
 }
