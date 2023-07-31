cbae56149a7a001c619c73bf1629a00fec7e53a0
Youssef <youssef@email>
2023-07-31 12:38:31 -0700

    Debugging in C programming

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring.
+ *
+ * @haystack: a character we search in for a substring
+ * @needle: the substring we look for it
+ *
+ * Return:a substring
+ **/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int num1, num2;
+	char *pr;
+
+	for (num1 = 0; haystack[num1] != '\0'; num1++)
+	{
+		for (num2 = 0; needle[num2] != '\0'; num2++)
+		{
+			if (needle[num2] == haystack[num1])
+			{
+				pr = &haystack[num1];
+				return (pr);
+			}
+		}
+	}
+
+	return (0);
+}
