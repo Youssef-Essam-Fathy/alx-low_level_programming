f3ee3e6356ec20a29cfbbb3c3b4b13669c20782b
Youssef <youssef@email>
2023-08-02 11:23:28 -0700

    Debugging in C programming

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ *
+ * @s: a string
+ *
+ * Return: void
+ **/
+
+void _puts_recursion(char *s)
+{
+	int n;
+
+	_putchar(s * s[n - 1]);
+}
