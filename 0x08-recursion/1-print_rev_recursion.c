3e6ba1e05f6b4cf0841fb6878994d5520b70e125
Youssef <youssef@email>
2023-08-02 13:59:29 -0700

    Debugging in C programming

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ *
+ * @s: a string
+ *
+ * Return: void
+ **/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		 _print_rev_recursion(s + 1);
+		 _putchar(*s);
+	}
+}
