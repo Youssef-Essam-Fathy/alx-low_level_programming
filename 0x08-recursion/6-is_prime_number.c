36619d83cb241b633dcebc1ff407eecfc567ae06
Youssef <youssef@email>
2023-08-02 16:12:09 -0700

    Debugging in C programming

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,13 +4,33 @@
  * is_prime_number -  returns 1 if the input integer is a prime number,
  *			otherwise return 0
  *
- * @othr
+ * @othrin: integer
  * @in: number to be checked
  *
  * Return: 1 or 0
  **/
 
-int chk_prim(int in, int othr);
+int chk_prim(int in, int othrin);
 int is_prime_number(int n)
 {
+	return (chk_prim(in, 2));
+}
+
+/**
+ * chk_prim - check all numbers < in if they could divide it
+ *
+ * @in: integer
+ * @othrin: integer
+ *
+ * Return: integer value
+ **/
+
+int chk_prim(int in, int othrin)
+{
+	if (othrin >= in && in > 1)
+		return (1);
+	else if (in % othrin == 0 || in <= 1)
+		return (0);
+	else
+		return (chk_prim(in, othrin + 1));
 }
