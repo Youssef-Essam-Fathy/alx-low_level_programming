04e3f27ef6ff945929a5093c03cbb926cf78f56b
Youssef <youssef@email>
2023-08-02 19:48:54 -0700

    Debugging in C programming

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -44,7 +44,7 @@ int wildcmp(char *s1, char *s2)
 {
 	int t = 0;
 
-	if (!*s1 && *s2 == '*' && !move_past_star(s2))
+	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
 		return (1);
 	if (*s1 == *s2)
 	{
