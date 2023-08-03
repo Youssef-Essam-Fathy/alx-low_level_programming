0554cfaf8271ccf70e9e0af6aaad090e0dd4ead5
Youssef <youssef@email>
2023-08-02 17:12:21 -0700

    Debugging in C programming

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,7 +1,59 @@
 #include "main.h"
 
-/***/
+/**
+ * lst_indx - returns the last index of a string
+ *
+ * @s: a string pointer
+ *
+ * Return: integer value
+ **/
+
+int is_palindrome(char *s);
+int chk(char *str, int strt, int endd, int modd);
+int lst_indx(char *str)
+{
+	int num = 0;
+
+	if (*str > '\0')
+	{
+		n += lst_indx(s + 1) + 1;
+	}
+
+	return (n);
+}
+
+/**
+ * is_palindrome - checks if the string is a palindrome
+ *
+ * @s: a string
+ *
+ * Return: integer value
+ **/
 
 int is_palindrome(char *s)
 {
+	int endd = lst_indx(s);
+
+	return (chk(s, 0, endd - 1, endd % 2));
+}
+
+/**
+ * chk - checking for the palindrome
+ *
+ * @str: string
+ * @strt: integer start value
+ * @endd: integer end value
+ * @modd: the % value
+ *
+ * Return: 0 or 1
+ **/
+
+int chk(char *str, int strt, int endd, int modd)
+{
+	if ((strt == endd && modd != 0) || (strt == endd + 1 && modd == 0))
+		return (1);
+	else if (str[strt] != str[endd])
+		return (0);
+	else
+		return (chk(str, strt + 1, endd - 1, modd));
 }
