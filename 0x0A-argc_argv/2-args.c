1fa92b58702e1a6db62dae6ab37e0e9680657ec6
Youssef <youssef@email>
2023-08-03 21:21:58 -0700

    Static libraries

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 
+/**
+ * main - entry point
+ * @argc: count of cmd_line args
+ * @argv: array pointer
+ * Return: always 0 (Succes)
+ **/
 
+int main(int argc, char *argv[])
+{
+	int indx;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[indx]);
+		indx++;
+	}
+
+	return (0);
+}
