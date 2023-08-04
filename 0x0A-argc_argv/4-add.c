4e8e856e82d471da9346713bc2b9bfed4a198365
Youssef <youssef@email>
2023-08-04 05:11:15 -0700

    Static libraries

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: count of cmd_line args
+ * @argv: array pointer
+ * Return: always 0 (Succes)
+ **/
+
+int main(int argc, char *argv[])
+{
+}
