46a9e90ed1da002426a34c16c0b0876969d65d0c
Youssef <youssef@email>
2023-08-04 05:35:18 -0700

    Static libraries

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,4 +10,19 @@
 
 int main(int argc, char *argv[])
 {
+	int add = 0;
+	char *chr;
+
+	while (--argc)
+	{
+		for (chr = argv[argc]; *chr; chr++)
+		{
+			if (*chr < '0' || *chr > '9')
+				return (printf("Error\n"), 1);
+			add += atoi(argv[argc]);
+		}
+	}
+
+	printf("%d\n", add);
+	return (0);
 }
