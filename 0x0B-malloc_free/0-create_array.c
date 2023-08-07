5e6f8c9c7c176e15ac88b55b2f3e2ff09d06c151
Youssef <youssef@email>
2023-08-07 14:32:34 -0700

    Static libraries

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars,
+ *		and initializes it with a specific char
+ * @size: integer parameter showing size of array
+ * @c: character
+ * Return: character pointer
+ **/
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+
+	if (size == 0)
+		return (NULL);
+	else
+		return (arr[c]);
+}
