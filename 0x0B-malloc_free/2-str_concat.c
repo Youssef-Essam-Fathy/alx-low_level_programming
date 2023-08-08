ff098e8da0d8d11bc20f8cb5057e4a476f87f457
Youssef <youssef@email>
2023-08-08 05:37:30 -0700

    free and malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,9 +29,9 @@ char *str_concat(char *s1, char *s2)
 	char *arr;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "\0";
 
 	len1 = str_len(s1);
 	len2 = str_len(s2);
