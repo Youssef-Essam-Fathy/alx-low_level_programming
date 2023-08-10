3f30ece04b0fda7c6d3cb71b053d4b8c81ceb9a2
Youssef <youssef@email>
2023-08-10 13:34:43 -0700

    more free and malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	length = max - min + 1;
-	p = malloc(sizeof(int) * len);
+	p = malloc(sizeof(int) * length);
 	if (!p)
 		return (NULL);
 	for (indx = 0; indx < length; indx++)
