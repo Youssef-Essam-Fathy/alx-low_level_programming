6bdbaa79a44ef86042a9c3060e72e530ae07c212
Youssef <youssef@email>
2023-08-10 19:40:33 -0700

    more free and malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,114 +1,90 @@
 #include "main.h"
-#include <stdio.h>
 #include <string.h>
 
 /**
- * _isdigit - checks if character is digit
- * @c: the character to check
- *
- * Return: 1 if digit, 0 otherwise
- */
-int _isdigit(int c)
+ * _puts - prints out a string
+ *	followed by a new line
+ * @st: string
+ * Return: void
+ **/
+
+void _puts(char *st)
 {
-	return (c >= '0' && c <= '9');
-}
+	int indx = 0;
 
-/**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
- * big_multiply - multiply two big number strings
- * @s1: the first big number string
- * @s2: the second big number string
- *
- * Return: the product big number string
- */
-char *big_multiply(char *s1, char *s2)
-{
-	char *r;
-	int l1, l2, a, b, c, x;
-
-	l1 = _strlen(s1);
-	l2 = _strlen(s2);
-	r = malloc(a = x = l1 + l2);
-	if (!r)
-		printf("Error\n"), exit(98);
-	while (a--)
-		r[a] = 0;
-
-	for (l1--; l1 >= 0; l1--)
+	while (st[indx])
 	{
-		if (!_isdigit(s1[l1]))
-		{
-			free(r);
-			printf("Error\n"), exit(98);
-		}
-		a = s1[l1] - '0';
-		c = 0;
-
-		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
-		{
-			if (!_isdigit(s2[l2]))
-			{
-				free(r);
-				printf("Error\n"), exit(98);
-			}
-			b = s2[l2] - '0';
-
-			c += r[l1 + l2 + 1] + (a * b);
-			r[l1 + l2 + 1] = c % 10;
-
-			c /= 10;
-		}
-		if (c)
-			r[l1 + l2 + 1] += c;
+		_putchar(st[indx]);
+		indx++;
 	}
-	return (r);
 }
 
+/**
+ * _atoi - convert the string to integer
+ * @str: string
+ * Return: integer value
+ **/
+
+int _atoi(const char *str)
+{
+	int sin = 1;
+	unsigned long int rsp = 0, frst_num, indx;
+
+	for (frst_num = 0; !(str[frst_num] >= 48 && str[frst_num] <= 57); frst_num++)
+	{
+		if (str[frst_num] == '-')
+		{
+			sin *= -1;
+		}
+	}
+
+	for (indx = frst_num; str[indx] >= 48 && str[indx] <= 57; indx++)
+	{
+		rsp *= 10;
+		rsp += (str[indx] - 48);
+	}
+
+	return (sin * rsp);
+}
 
 /**
- * main - multiply two big number strings
- * @argc: the number of arguments
- * @argv: the argument vector
- *
- * Return: Always 0 on success.
- */
-int main(int argc, char **argv)
+ * print_int - print integer
+ * @num: integer
+ * Return: void
+ **/
+
+void print_int(unsigned long int num)
 {
-	char *r;
-	int a, c, x;
+	unsigned long int div = 1, indx, rsp;
+
+	for (indx = 0; num / div > 9; indx++, div *= 10)
+		;
+
+	for (; div >= 1; num %= div, div /= 10)
+	{
+		rsp = num / div;
+		_putchar('0' + rsp);
+	}
+}
+
+/**
+ * main - Entry point
+ * @argc: arguments count
+ * @argv: arguments value
+ * Return: always 0 (Success)
+ **/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
 
 	if (argc != 3)
-		printf("Error\n"), exit(98);
-
-	x = _strlen(argv[1]) + _strlen(argv[2]);
-	r = big_multiply(argv[1], argv[2]);
-	c = 0;
-	a = 0;
-	while (c < x)
 	{
-		if (r[c])
-			a = 1;
-		if (a)
-			_putchar(r[c] + '0');
-		c++;
+		_puts("Error");
+		exit(98);
 	}
-	if (!a)
-		_putchar('0');
+	print_int(_atoi(argv[1]) * _atoi(argv[2]));
 	_putchar('\n');
-	free(r);
+
 	return (0);
 }
