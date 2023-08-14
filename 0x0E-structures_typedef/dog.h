dc577b5161429a117427c01d2a10626074ebce31
Youssef <youssef@email>
2023-08-14 16:11:02 -0700

    structures and typeedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog struct
+ * @name: dog  name
+ * @age: dog age
+ * @owner: dog owner
+ * Description: alone dog struct
+ **/
+
 struct dog
- {
+{
 	 char *name;
 	 float age;
 	 char *owner;
- };
+};
 
 #endif /* !DOG_H */
