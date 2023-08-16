e5aaf1dc3203e13781a2e402aa5094cb3eefcb94
Youssef <youssef@email>
2023-08-16 03:46:42 -0700

    structures and typeedef

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name -  prints a name
+ * @name: character
+ * @f: pointer to print_name function
+ * Return: void
+ **/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f = &print_name;
+	f(name);
+}
