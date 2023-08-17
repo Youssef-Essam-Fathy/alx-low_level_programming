66d6962df6bbb674c65337ee744a5a4ae69b5b25
Youssef <youssef@email>
2023-08-16 18:51:53 -0700

    Function pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * main- Entry point
+ * @argc: arguments count
+ * @argv: argumennts value
+ * Return: always 0 (Success)
+ **/
+
+int main(int argc, char **argv)
+{
+	int *ptr = (char *) main;
+	int bytes;
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+		printf("Error\n"), exit(2);
+
+	while (b--)
+		printf("%02hhx%s", *ptr++, bytes ? " " : "\n");
+	return (0);
+}
