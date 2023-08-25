08fd564f31d68e93cd0e8063f1a1bf26e6a7c197
Youssef <youssef@email.com>
2023-08-24 17:58:17 -0700

    Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+void _constructor(void) __attribute__ ((constructor));
+
+/**
+ * _constructor : implemented before main()
+ * Return: void
+ **/
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
