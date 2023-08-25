1526ee4428862cdaa7ab8189e9a9e4a434a3f289
Youssef <youssef@email.com>
2023-08-24 18:11:51 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,10 +3,10 @@ section		.txt
 	global	main
 
 main:
-	mov		edi, req
+	mov		edi, msg
 	mov		eax, 0
 	call	printf
 
 
 section		.data
-	req db 'Hello, Holberton', 0xa, 0
+	msg db 'Hello, Holberton', 0xa, 0
