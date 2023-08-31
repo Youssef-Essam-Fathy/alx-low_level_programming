7b29c17d12ec13f2f16e4ee9d2db74f3699099ef
Youssef <youssef@email.com>
2023-08-31 02:29:41 -0700

    More singly linked lists

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,7 +11,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
 		return (-1);
-        if (*n & 1L << index)
-                *n ^= 1L << index;
-        return (1);
+	if (*n & 1L << index)
+		*n ^= 1L << index;
+	return (1);
 }
