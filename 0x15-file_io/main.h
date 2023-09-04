eb0ddf48418010baf7ca2b6cee246e7c7030e915
Youssef <youssef@email.com>
2023-09-04 16:56:39 -0700

    File I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define READ_BUFF_SIZE 1024
+
+/* prototypes */
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
