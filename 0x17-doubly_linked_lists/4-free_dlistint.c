2a27acd6bca39a78c5d8e9064ff8081bb0f72f3d
youssef_esssam_fathy <youssefessam5623@gmail.com>
2023-11-09 16:55:49 -0800

    0x17-doubly_linked_lists

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free thhe  linkedd list
+ * @head: poiinter to head of the list
+ * Return: void
+ **/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint *nod;
+
+	while(head)
+	{
+		nod = head;
+		head
+	}
+}
