3863032e3421ed92bd74e34f3f5249cdd7c34856
Youssef <youssef@email.com>
2023-12-01 23:47:46 +0200

    0x1A-hash_tables

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - a function that retrieves a value associated with a key
+ * @ht:  the hash table you want to look into
+ * @key: the key you are looking for
+ * Return:  the value associated with the element,
+ *          or NULL if key couldn’t be found
+ **/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int indx;
+	hash_node_t *temp;
+
+	if (!ht || !ht->array || ht->size == 0 ||
+		!key || strlen(key) == 0)
+		return (0);
+	indx = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[indx];
+	while (!temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,5 +40,6 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 hash_node_t *make_hash_node(const char *key, const char *value);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 
 #endif /* HASH_TABLES_H */
