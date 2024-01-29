8ffdfe05d0c713e13311f02180c53c421aa5fafa
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-01-29 13:24:57 +0200

    0x1C-makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-"""5-island_perimeter module"""
+"""No module imported"""
 
 
 def island_perimeter(grid):
-    """Returns the perimeter of the island described in grid"""
-    c = 0
+    """ a function that returns
+        the perimeter of the island
+        described in grid"""
+    count = 0
     length = len(grid) - 1
     width = len(grid[0]) - 1
 
     for i, r in enumerate(grid):
-        for j, n in enumerate(r):
-            if n == 1:
+        for j, f in enumerate(r):
+            if f == 1:
                 if i == 0 or grid[i - 1][j] != 1:
-                    c += 1
+                    count += 1
                 if j == 0 or grid[i][j - 1] != 1:
-                    c += 1
+                    count += 1
                 if j == width or grid[i][j + 1] != 1:
-                    c += 1
+                    count += 1
                 if i == length or grid[i + 1][j] != 1:
-                    c += 1
-    return c
+                    count += 1
+    return count
